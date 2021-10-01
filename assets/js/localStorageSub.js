09e0000c2782978ec30bbef1c191e5239dd45232
Wickette <stephany.clark2@gmail.com>
2021-10-01 16:47:18 -0400

    Added localStorage component to subscription section and removed from mealGenerator

---
+++ assets/js/localStorageSub.js
@@ -0,0 +1,19 @@
+let emailInput = document.querySelector("#email")
+let sub = document.querySelector("form")
+let subBtn = document.querySelector(".subButton")
+
+function save (){
+    let emailEntry = emailInput.value.trim();
+    console.log(emailEntry)
+     localStorage.setItem("email", emailEntry);
+}
+
+function view(){
+        sub.innerHTML = "Thank you for subscribing" + " " + localStorage.getItem("data");  
+}
+
+subBtn.addEventListener("click", function(){
+    save();
+    view();
+})
+
